ac168b26839acff45176fd0aadf46787975b3442
DominikKossinski <dominikkossinski@gmail.com>
2021-03-10 23:17:44 +0100

    loading data, getting simple stats from data

---
+++ run.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import json
+import os
+
+
+def main():
+    gb_videos = pd.read_csv(os.path.join("youtube_data", "GB_videos_5p.csv"), sep=";")
+    print(gb_videos.head(20))
+    print(gb_videos.shape)
+    print(gb_videos.columns)
+    print(gb_videos.dtypes)
+    with open(os.path.join("youtube_data", "GB_category_id.json"), "r") as file:
+        gb_category = json.load(file)
+        file.close()
+    print(len(gb_category["items"]))
+
+
+if __name__ == '__main__':
+    main()
