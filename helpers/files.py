9e385198d5598f31daa0fd1a68b5a28188a684cf
DominikKossinski <dominikkossinski@gmail.com>
2021-04-18 12:51:32 +0200

    most frequent tokens count generated from data

--- helpers/files.py
+++ helpers/files.py
@@ -1,8 +1,20 @@
 import os
 from typing import List
 
+import pandas as pd
+
 
 def get_csv_files(dir_path: str) -> List[str]:
     if not os.path.isdir(dir_path):
         raise ValueError("Path is not directory")
     return list(filter(lambda x: x.endswith(".csv"), os.listdir(dir_path)))
+
+
+def save_csv(dir_name: str, frames: List[pd.DataFrame], names: List[str]) -> None:
+    if len(frames) != len(names):
+        ValueError("Number of frames not equal number of names")
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, "..", dir_name)
+    os.makedirs(path, exist_ok=True)
+    for df, name in zip(frames, names):
+        df.to_csv(os.path.join(path, f"{name}.csv"), sep=";")
--- helpers/json_helper.py
+++ helpers/json_helper.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import List
 
 
@@ -9,7 +10,25 @@ def load_tokenized_text(path: str) -> List[List[str]]:
     return tokenized
 
 
-def save_grouped_tokenized(path: str, data: List[List[List[str]]]):
+def save_grouped_tokenized(path: str, data: List[List[List[str]]]) -> None:
     with open(path, "w") as file:
         json.dump(data, file)
         file.close()
+
+
+def save_frequent_tokens_dict(name: str, frequent_tokens: dict) -> None:
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, "..", "frequent_tokens")
+    os.makedirs(path, exist_ok=True)
+    with open(os.path.join(path, f"{name}.json"), "w") as file:
+        json.dump(frequent_tokens, file)
+        file.close()
+
+
+def load_frequent_tokens_dict(name: str) -> dict:
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, "..", "frequent_tokens")
+    with open(os.path.join(path, f"{name}.json"), "r") as file:
+        frequent_tokens = json.load(file)
+        file.close()
+    return frequent_tokens
