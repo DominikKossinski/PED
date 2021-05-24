539e0d3f60f1308678d80949f04b5f76503da583
DominikKossinski <dominikkossinski@gmail.com>
2021-05-24 23:59:14 +0200

    toto update

---
+++ ped5/download_videos_data.py
@@ -0,0 +1,5 @@
+# TODO regoinCode: The parameter value is an ISO 3166-1 alpha-2 country code. (string), GB and US
+# TODO publishDate: as in original dataset
+# TODO trendingDate: as in original dataset
+# TODO maybe chart: mostPopular
+# TODO using api key
